6a5e927757e07c6c73450dbb97ad0933be531607
Melkamu-Kassie <melkamukassie27@gmail.com>
2024-05-11 19:24:53 +0300

    add the code

---
+++ backend/index.jsx
@@ -0,0 +1,9 @@
+const index = () => {
+  return (
+    <div>
+      gfgfgfghf
+    </div>
+  )
+}
+
+export default index
---
+++ frontend/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+      hgjkhkhkh
+    </div>
+  )
+}
+
+export default index
